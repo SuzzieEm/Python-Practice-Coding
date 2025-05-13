21726962f96e03a127015c4a1ee57b71b3b2ffb5
SuzzieEm <flashysusanna@yahoo.com>
2025-05-13 12:08:48 -0700

    First Python test on lesson on variables

---
+++ Prac_Variables.py
@@ -0,0 +1,9 @@
+name = input("Enter name: ")
+job_name = input("Enter program: ")
+first_adjective = input("Enter first adjective: ")
+second_adjective = input("Enter second adjective: ")
+first_food = input("Enter first food: ")
+second_food = input("Enter second food: ")
+a_feeling = input("Enter a feeling: ")
+print(f'''{name} started their first Generation course today. They are training as a {job_name}. They found their cohort to be very {first_adjective} but their teacher was, at least, {second_adjective}. For lunch they have {first_food} and {second_food} while reviewing their notes. They feel {a_feeling} but are determined to complete the course.''')
+
