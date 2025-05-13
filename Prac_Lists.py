b9e36d63883b112fd29a2d40d6fb04674d8264bf
SuzzieEm <flashysusanna@yahoo.com>
2025-05-13 13:02:55 -0700

    Practicing creating lists in python

---
+++ Prac_Lists.py
@@ -0,0 +1,5 @@
+list_item_1 = input("please input first item.")
+list_item_2 = input("please input next item.")
+list_item_3 = input("please input final item.")
+print("Hi, you selected", [list_item_1,list_item_2,list_item_3],".", "Please proceed to checkout.", "Thanks for shopping!")
+
